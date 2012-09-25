59f96651d7528e4f6cfffde0c87b61462d7cdb1a
Roland Moriz <roland@moriz.de>
2012-09-25 18:13:58 +0200

    undo DRY-change (keep it consistent, cleanup later)

--- lib/hetzner/api/boot/linux.rb
+++ lib/hetzner/api/boot/linux.rb
@@ -3,20 +3,18 @@ module Hetzner
     module Boot
       module Linux
         def boot_linux?(ip)
-          perform_get boot_linux_path(ip)
+          path = "/boot/#{ip}/linux"
+          perform_get path
         end
 
         def boot_linux!(ip, dist, arch, lang)
-          perform_post boot_linux_path(ip), :query => {:dist => dist, :arch => arch, :lang => lang }
+          path = "/boot/#{ip}/linux"
+          perform_post path, :query => {:dist => dist, :arch => arch, :lang => lang }
         end
 
         def disable_boot_linux!(ip)
-          perform_delete boot_linux_path(ip)
-        end
-
-      private
-        def boot_linux_path(ip)
-          "/boot/#{ip}/linux"
+          path = "/boot/#{ip}/linux"
+          perform_delete path
         end
       end
     end
